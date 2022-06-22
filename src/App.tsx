e13f1064fd88dffcede5af4809e181172568e4af
Jaime <jaime251102@gmail.com>
2022-06-22 10:04:52 -0400

    prueba 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+type FormElemet = React.FormEvent<HTMLFormElement>;
 
-function App() {
+interface ITareas {
+  nombre: string;
+  done: boolean;
+}
+
+function App(): JSX.Element {
+  const [nuevaTarea, setnuevaTarea] = useState<string>("");
+  const [tareas, settareas] = useState<ITareas[]>([]);
+  const tasInput = useRef<HTMLInputElement>(null);
+
+  const ButtonSubmit = (e: FormElemet): void => {
+    e.preventDefault();
+    AgregarTarea(nuevaTarea);
+    setnuevaTarea("");
+    tasInput.current?.focus();
+  };
+
+  const AgregarTarea = (nombre: string): void => {
+    const nuevasTareas: ITareas[] = [...tareas, { nombre, done: false }];
+    settareas(nuevasTareas);
+  };
+
+  const CambiarCheck = (i: number): void => {
+    const nuevaTarea: ITareas[] = [...tareas];
+    nuevaTarea[i].done = !nuevaTarea[i].done;
+    settareas(nuevaTarea);
+  };
+
+  const EliminarTarea = (i: number): void => {
+    const nuevastaeas: ITareas[] = [...tareas];
+    nuevastaeas.splice(i, 1);
+    settareas(nuevastaeas);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container p-4">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <div className="card">
+            <div className="card-body">
+              <form onSubmit={ButtonSubmit}>
+                <input
+                  type="text"
+                  onChange={(e) => setnuevaTarea(e.target.value)}
+                  value={nuevaTarea}
+                  className="form-control"
+                  ref={tasInput}
+                  autoFocus
+                />
+                <button className="btn btn-secondary btn-block mt-2">
+                  Mandar
+                </button>
+              </form>
+            </div>
+          </div>
+
+          {tareas.map((t: ITareas, i: number) => (
+            <div className=" card card-body mt-2" key={i}>
+              <h2 style={{ textDecoration: t.done ? "line-through" : "" }}>
+                {t.nombre}
+              </h2>
+              <div>
+                <button
+                  className="btn btn-secundary"
+                  onClick={() => CambiarCheck(i)}
+                >
+                  {t.done ? "✓" : "✕"}
+                </button>
+                <button
+                  className="btn btn-secundary"
+                  onClick={() => EliminarTarea(i)}
+                >
+                  🗑
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
